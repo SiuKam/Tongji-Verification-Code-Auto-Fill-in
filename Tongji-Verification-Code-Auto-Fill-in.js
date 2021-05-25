8cd98bb1fa3e7d7406dd0d0512b8ce190bfe8bd7
SiuKam <41332305+SiuKam@users.noreply.github.com>
2021-05-25 11:26:31 +0800

    Archive note

--- Tongji-Verification-Code-Auto-Fill-in.js
+++ Tongji-Verification-Code-Auto-Fill-in.js
@@ -1,13 +1,15 @@
 // ==UserScript==
 // @name         同济大学统一身份认证自动填充验证码
 // @namespace    https://greasyfork.org/zh-CN/scripts/375971
-// @version      0.3
+// @version      0.3.1
 // @description  自动填充同济大学统一验证页面的验证码
 // @author       SiucheukKam
 // @match        https://ids.tongji.edu.cn:8443/*
 // @grant        none
 // ==/UserScript==
 
-(function() {
-    document.forms['IDPLogin']['Txtidcode'].value = String(document.getElementById('ehong-code').innerText);
-})();
+// Removed due to a change of verification method.
+
+// (function() {
+//     document.forms['IDPLogin']['Txtidcode'].value = String(document.getElementById('ehong-code').innerText);
+// })();
